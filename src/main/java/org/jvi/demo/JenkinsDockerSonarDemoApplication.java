eac83b3debd7722cb68d7b8688fe86d440b3109d
Julien Vienne <vienne.julien@gmail.com>
2018-09-18 20:57:21 +0200

    Initial commit

---
+++ src/main/java/org/jvi/demo/JenkinsDockerSonarDemoApplication.java
@@ -0,0 +1,12 @@
+package org.jvi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerSonarDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerSonarDemoApplication.class, args);
+	}
+}
